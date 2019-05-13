eb746d38fa49857f2d151355653b64fe92cb295a
SpiveeWorks <spiveehere@gmail.com>
2019-05-13 20:19:49 +1000

    add a platform

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ fn main() {
         f(Vertex { position: [x-0.5, y-0.5, z+1.0], color: [0.9, 0.9, 0.9] });
     }
 
-    let mut cube_pos = [
+    let mut cube_pos = vec![
         [0.0, 0.0, 0.0],
         [-1.0, 0.0, 0.0],
         [-2.0, 0.0, 0.0],
@@ -134,6 +134,11 @@ fn main() {
         [0.0, 0.0, 1.0],
         [0.0, 0.0, 2.0],
     ];
+    for i in -10..=10 {
+        for j in -10..=10 {
+            cube_pos.push([i as f32, j as f32, -1.0]);
+        }
+    }
     // this will sort primarily by z
     cube_pos.sort_by(|[_x1, _y1, _z1], [_x2, _y2, _z2]|
         PartialOrd::partial_cmp(_x2, _x1).unwrap());
